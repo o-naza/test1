f651d62165501cfb4f010e865d53477b11826a78
Nazar <nazarorlov552@gmail.com>
2025-03-10 22:23:00 +0200

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,16 +37,6 @@ export default defineConfig({
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"], viewport: { width: 1920, height: 1080 } },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"], viewport: { width: 1920, height: 1080 }  },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"], viewport: { width: 1920, height: 1080 }  },
-    },
+    }
   ],
 });
